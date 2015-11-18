f02517a39742814a21202deda83dcbdb86b60497
Jimmy Kiselak <jpk@jimmykiselak.com>
2015-11-18 18:53:35 -0500

    auto-update the setup script

--- lbry_setup.sh
+++ lbry_setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-exec >  >(tee -a setup.log)
-exec 2> >(tee -a setup.log >&2)
+exec >  >(tee -a script_setup.log)
+exec 2> >(tee -a script_setup.log >&2)
 
 ROOT=.
 GIT_URL_ROOT="https://github.com/lbryio/"
-CONF_DIR=~/.lbrycrd
-CONF_PATH=$CONF_DIR/lbrycrd.conf
-PACKAGES="git libgmp3-dev build-essential python2.7 python2.7-dev python-pip"
+PACKAGES="git"
 
 #install/update requirements
 if hash apt-get 2>/dev/null; then
@@ -17,98 +15,29 @@ else
 	printf "Running on a system without apt-get. Install requires the following packages or equivalents: $PACKAGES\n\nPull requests encouraged if you have an install for your system!\n\n"
 fi
 
-#create config file
-if [ ! -f $CONF_PATH ]; then
-	printf "Adding lbry config in $CONF_PATH\n";
-	mkdir -p $CONF_DIR
-	printf "rpcuser=lbryrpc" > $CONF_PATH
-	printf "\nrpcpassword=" >> $CONF_PATH
-	tr -dc A-Za-z0-9 < /dev/urandom | head -c ${1:-12} | xargs >> $CONF_PATH 
-else
-	printf "Config $CONF_PATH already exists\n";
-fi
-
 #Clone/pull repo and return true/false whether or not anything changed
 #$1 : repo name
 UpdateSource() 
 {
-	if [ ! -d "$ROOT/$1/.git" ]; then
-       		printf "$1 does not exist, checking out\n";
-	        git clone "$GIT_URL_ROOT$1.git"
+	if [ ! -d "$ROOT/.git" ]; then
+       		printf "setup script does not exist, checking out\n";
+	        git clone "${GIT_URL_ROOT}lbry-setup.git" .
 		return 0 
 	else
-		cd $1
 		#http://stackoverflow.com/questions/3258243/git-check-if-pull-needed
 		git remote update;
 		LOCAL=$(git rev-parse @{0})
 		REMOTE=$(git rev-parse @{u})
 		if [ $LOCAL = $REMOTE ]; then
-			printf "No changes to $1 source\n"
-            cd ..
+			printf "No changes to setup script\n"
 			return 1 
 		else
-			printf "Fetching source changes to $1\n"
+			printf "Fetching source changes to setup script\n"
 			git pull --rebase
-            cd ..
 			return 0
 		fi
 	fi
 }
+UpdateSource
 
-if [ ! -d bin ]; then
-    printf "Creating bin\n"
-    mkdir -p bin
-else
-    printf "bin directory already exists\n"
-fi
-
-UPDATELBRYCRD=0
-
-if [ -e bin/lbrycrd.tar.gz ]; then
-    LBRYCRDHASHSUM=`md5sum bin/lbrycrd.tar.gz | awk '{print $1}'`
-    if [ ! "$LBRYCRDHASHSUM" = "0476f132e30bdc2839a830145833b77d" ] && [ ! "$LBRYCRDHASHSUM" = "5cbc04df818d12db731ddfd55deb4224" ]; then
-        UPDATELBRYCRD=1
-    fi
-else
-    UPDATELBRYCRD=1
-fi
-
-if [ $UPDATELBRYCRD = 1 ]; then
-    cd bin
-    if [ `getconf LONG_BIT` = "64" ]; then
-        wget https://github.com/lbryio/lbrycrd/releases/download/v0.1-alpha/lbrycrd_64.tar.gz -O lbrycrd.tar.gz
-    else
-        wget https://github.com/lbryio/lbrycrd/releases/download/v0.1-alpha/lbrycrd_32.tar.gz -O lbrycrd.tar.gz
-    fi
-    tar xf lbrycrd.tar.gz
-    mv lbrycrd/* .
-    rm -rf lbrycrd
-    cd ..
-    if [ -e ~/.lbrycrddpath.conf ]; then
-        if [ `cat ~/.lbrycrddpath.conf` = "`pwd`/lbrycrd/src/lbrycrdd" ]; then
-            rm ~/.lbrycrddpath.conf
-        fi
-    fi
-else
-	printf "lbrycrd installed and nothing to update\n"
-fi
-
-if [ ! -e ~/.lbrycrddpath.conf ]; then
-    echo `pwd`/bin/lbrycrdd > ~/.lbrycrddpath.conf
-fi
-#setup lbry-console
-printf "\n\nInstalling/updating lbry-console\n";
-if UpdateSource lbry || [ ! -d $ROOT/lbry/dist ]; then
-	printf "Running lbry-console setup\n"
-	cd lbry
-    if [ -d dist ]; then
-        if [ `stat -c "%U" dist` = "root" ]; then
-            sudo rm -rf dist build ez_setup.pyc lbrynet.egg-info setuptools-4.0.1-py2.7.egg setuptools-4.0.1.zip
-        fi
-    fi
-    python2.7 setup.py build bdist_egg
-	sudo python2.7 setup.py install
-	cd ..
-else
-	printf "lbry-console installed and nothing to update\n"
-fi
+./update_lbry.sh
---
+++ update_lbry.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+exec >  >(tee -a setup.log)
+exec 2> >(tee -a setup.log >&2)
+
+ROOT=.
+GIT_URL_ROOT="https://github.com/lbryio/"
+CONF_DIR=~/.lbrycrd
+CONF_PATH=$CONF_DIR/lbrycrd.conf
+PACKAGES="git libgmp3-dev build-essential python2.7 python2.7-dev python-pip"
+
+#install/update requirements
+if hash apt-get 2>/dev/null; then
+	printf "Installing $PACKAGES\n\n"
+	sudo apt-get install $PACKAGES
+else
+	printf "Running on a system without apt-get. Install requires the following packages or equivalents: $PACKAGES\n\nPull requests encouraged if you have an install for your system!\n\n"
+fi
+
+#create config file
+if [ ! -f $CONF_PATH ]; then
+	printf "Adding lbry config in $CONF_PATH\n";
+	mkdir -p $CONF_DIR
+	printf "rpcuser=lbryrpc" > $CONF_PATH
+	printf "\nrpcpassword=" >> $CONF_PATH
+	tr -dc A-Za-z0-9 < /dev/urandom | head -c ${1:-12} | xargs >> $CONF_PATH 
+else
+	printf "Config $CONF_PATH already exists\n";
+fi
+
+#Clone/pull repo and return true/false whether or not anything changed
+#$1 : repo name
+UpdateSource() 
+{
+	if [ ! -d "$ROOT/$1/.git" ]; then
+       		printf "$1 does not exist, checking out\n";
+	        git clone "$GIT_URL_ROOT$1.git"
+		return 0 
+	else
+		cd $1
+		#http://stackoverflow.com/questions/3258243/git-check-if-pull-needed
+		git remote update;
+		LOCAL=$(git rev-parse @{0})
+		REMOTE=$(git rev-parse @{u})
+		if [ $LOCAL = $REMOTE ]; then
+			printf "No changes to $1 source\n"
+            cd ..
+			return 1 
+		else
+			printf "Fetching source changes to $1\n"
+			git pull --rebase
+            cd ..
+			return 0
+		fi
+	fi
+}
+
+if [ ! -d bin ]; then
+    printf "Creating bin\n"
+    mkdir -p bin
+else
+    printf "bin directory already exists\n"
+fi
+
+UPDATELBRYCRD=0
+
+if [ -e bin/lbrycrd.tar.gz ]; then
+    LBRYCRDHASHSUM=`md5sum bin/lbrycrd.tar.gz | awk '{print $1}'`
+    if [ ! "$LBRYCRDHASHSUM" = "8adba4d53751636617853519e487ffeb" ] && [ ! "$LBRYCRDHASHSUM" = "84501f4eea172fc64f0b97e2956da0e1" ]; then
+        UPDATELBRYCRD=1
+    fi
+else
+    UPDATELBRYCRD=1
+fi
+
+if [ $UPDATELBRYCRD = 1 ]; then
+    cd bin
+    if [ `getconf LONG_BIT` = "64" ]; then
+        wget https://github.com/lbryio/lbrycrd/releases/download/v0.1-alpha/lbrycrd_64.tar.gz -O lbrycrd.tar.gz
+    else
+        wget https://github.com/lbryio/lbrycrd/releases/download/v0.1-alpha/lbrycrd_32.tar.gz -O lbrycrd.tar.gz
+    fi
+    tar xf lbrycrd.tar.gz
+    mv lbrycrd/* .
+    rm -rf lbrycrd
+    cd ..
+    if [ -e ~/.lbrycrddpath.conf ]; then
+        if [ `cat ~/.lbrycrddpath.conf` = "`pwd`/lbrycrd/src/lbrycrdd" ]; then
+            rm ~/.lbrycrddpath.conf
+        fi
+    fi
+else
+	printf "lbrycrd installed and nothing to update\n"
+fi
+
+if [ ! -e ~/.lbrycrddpath.conf ]; then
+    echo `pwd`/bin/lbrycrdd > ~/.lbrycrddpath.conf
+fi
+#setup lbry-console
+printf "\n\nInstalling/updating lbry-console\n";
+if UpdateSource lbry || [ ! -d $ROOT/lbry/dist ]; then
+	printf "Running lbry-console setup\n"
+	cd lbry
+    if [ -d dist ]; then
+        if [ `stat -c "%U" dist` = "root" ]; then
+            sudo rm -rf dist build ez_setup.pyc lbrynet.egg-info setuptools-4.0.1-py2.7.egg setuptools-4.0.1.zip
+        fi
+    fi
+    python2.7 setup.py build bdist_egg
+	sudo python2.7 setup.py install
+	cd ..
+else
+	printf "lbry-console installed and nothing to update\n"
+fi
